d01ca9f8b7b1769abd7c0c005453b581d696b4c9
ddt-hayata <ddt.jn.hayata@gmail.com>
2022-05-20 19:27:38 +0900

    演習4-6の課題が完了しました。

---
+++ ensyu/src/ensyu_4_6/ensyu_4_6_1.java
@@ -0,0 +1,49 @@
+/*
+ * 演習4-6
+ * 読み込んだ値が1未満であれば改行文字を出力しないように
+ * List4-7およびList4-8を書きかえたプログラムをそれぞれ作成せよ。
+ */
+package ensyu_4_6;
+
+import java.util.Scanner;
+
+public class ensyu_4_6_1 {
+
+	public static void main(String[] args) {
+		
+		// Scannerクラスのインスタンスを作成し、引数で標準入力System.inを指定
+		Scanner scanner = new Scanner(System.in);
+		
+		// コンソールに出力
+		System.out.print("何個*を表示しますか？：");
+		
+		// 変数に代入
+		int number = scanner.nextInt();
+		
+		// 変数の初期化
+		int i = 0;
+		
+		// ループ条件
+		
+		do { 
+			// コンソールに出力
+			System.out.print("");
+		
+		} while (number != 0);
+		
+		while (i < number) {
+			
+			// コンソールに出力
+			System.out.println('*');
+			
+			// 変数に加数
+			i++;
+			
+		}
+		
+		// コンソールに出力
+		//System.out.print("");
+		
+	}
+
+}
