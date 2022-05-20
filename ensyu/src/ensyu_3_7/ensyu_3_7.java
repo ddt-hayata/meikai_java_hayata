4d174f6237e6c3c3625059bace4b38764c37e9c5
ddt-hayata <ddt.jn.hayata@gmail.com>
2022-05-20 11:20:16 +0900

    演習3-7の課題が完了しました。

---
+++ ensyu/src/ensyu_3_7/ensyu_3_7.java
@@ -0,0 +1,41 @@
+package ensyu_3_7;
+
+import java.util.Scanner;
+
+public class ensyu_3_7 {
+
+	public static void main(String[] args) {
+		
+		// Scannerクラスのインスタンスを作成し、引数で標準入力System.inを指定
+		Scanner scanner = new Scanner(System.in);
+		
+		// コンソールに出力
+		System.out.print("整数値：");
+		
+		// 変数に代入
+		int number = scanner.nextInt();
+		
+		if (number > 0) {
+			if (number % 3 == 0) {
+				// コンソールに出力
+				System.out.println("その値は3で割り切れます。");
+			
+			} else if (number % 3 == 1) {
+				// コンソールに出力
+				System.out.println("その値を3で割ったあまりは1です。");
+			
+			} else {
+				// コンソールに出力
+				System.out.println("その値を3で割ったあまりは2です。");
+			
+			}
+			
+		} else {
+			// コンソールに出力
+			System.out.println("正でない値が入力されました。");
+			
+		}
+		
+	}
+
+}
