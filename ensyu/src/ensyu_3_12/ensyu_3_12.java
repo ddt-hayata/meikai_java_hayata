8c5c45ca0f70dfa791cfb1d2055fdb349db3dc0f
ddt-hayata <ddt.jn.hayata@gmail.com>
2022-05-20 11:27:04 +0900

    演習3-12の課題が完了しました。

---
+++ ensyu/src/ensyu_3_12/ensyu_3_12.java
@@ -0,0 +1,50 @@
+package ensyu_3_12;
+
+import java.util.Scanner;
+
+public class ensyu_3_12 {
+
+	public static void main(String[] args) {
+		
+		// Scannerクラスのインスタンスを作成し、引数で標準入力System.inを指定
+		Scanner scanner = new Scanner(System.in);
+		
+		// コンソールに出力
+		System.out.print("整数a：");
+		
+		// 変数に代入
+		int firstNumber = scanner.nextInt();
+		
+		// コンソールに出力
+			System.out.print("整数b：");
+		
+		// 変数に代入
+		int secondNumber = scanner.nextInt();
+		
+		// コンソールに出力
+		System.out.print("整数c：");
+				
+		// 変数に代入
+		int thirdNumber = scanner.nextInt();
+		
+		// 変数に代入
+		int min = firstNumber;
+		
+		if (secondNumber < min) {
+			// 変数に代入
+			min = secondNumber;
+		
+		}
+		
+		if (thirdNumber < min) {
+			// 変数に代入
+			min = thirdNumber;
+		
+		}
+		
+		// コンソールに出力
+		System.out.print("最小値は" + min + "です。");
+		
+	}
+
+}
