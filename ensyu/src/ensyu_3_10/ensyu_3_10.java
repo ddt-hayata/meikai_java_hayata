a93f73a124174bea3955caf80d880e7e65866f53
ddt-hayata <ddt.jn.hayata@gmail.com>
2022-05-20 11:24:33 +0900

    演習3-10の課題が完了しました。

---
+++ ensyu/src/ensyu_3_10/ensyu_3_10.java
@@ -0,0 +1,39 @@
+package ensyu_3_10;
+
+import java.util.Scanner;
+
+public class ensyu_3_10 {
+
+	public static void main(String[] args) {
+		
+		// Scannerクラスのインスタンスを作成し、引数で標準入力System.inを指定
+		Scanner scanner = new Scanner(System.in);
+		
+		// コンソールに出力
+		System.out.print("整数a：");
+		
+		// 変数に代入
+		int firstNumber = scanner.nextInt();
+		
+		// コンソールに出力
+			System.out.print("整数b：");
+		
+		// 変数に代入
+		int secondNumber = scanner.nextInt();
+		
+		// 変数に代入
+		int difference = firstNumber - secondNumber;
+		
+		if (difference > 0) {
+			// コンソールに出力
+			System.out.print("それらの差は" + difference + "です。");
+		
+		} else {
+			// コンソールに出力
+			System.out.print("それらの差は" + -difference + "です。");
+		
+		}
+		
+	}
+
+}
