cb08b8edb186f6525e635319821d8c9b49bf8121
ddt-hayata <ddt.jn.hayata@gmail.com>
2022-05-20 11:23:21 +0900

    演習3-9の課題が完了しました。

---
+++ ensyu/src/ensyu_3_9/ensyu_3_9.java
@@ -0,0 +1,32 @@
+package ensyu_3_9;
+
+import java.util.Scanner;
+
+public class ensyu_3_9 {
+
+	public static void main(String[] args) {
+		
+		// Scannerクラスのインスタンスを作成し、引数で標準入力System.inを指定
+		Scanner scanner = new Scanner(System.in);
+		
+		// コンソールに出力
+		System.out.print("実数a：");
+		
+		// 変数に代入
+		double firstNumber = scanner.nextDouble();
+		
+		// コンソールに出力
+			System.out.print("実数b：");
+		
+		// 変数に代入
+		double secondNumber = scanner.nextDouble();
+		
+		// 変数に代入
+		double max = firstNumber > secondNumber ? firstNumber : secondNumber;
+		
+		// コンソールに出力
+		System.out.print("大きい方の数値は" + max + "です。");
+		
+	}
+
+}
