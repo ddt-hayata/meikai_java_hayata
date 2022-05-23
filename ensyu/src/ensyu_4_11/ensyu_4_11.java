b5f31cfaea0b68fb108ce7973624cdfff5556bd9
ddt-hayata <ddt.jn.hayata@gmail.com>
2022-05-23 10:11:40 +0900

    演習4-11の課題が完了しました。

---
+++ ensyu/src/ensyu_4_11/ensyu_4_11.java
@@ -0,0 +1,29 @@
+/*
+ * 正の整数値を0までカウントダウンするList4-4（p.100）をfor文で実現せよ。
+ */
+package ensyu_4_11;
+
+import java.util.Scanner;
+
+public class ensyu_4_11 {
+
+	public static void main(String[] args) {
+
+		// インスタンス化
+		Scanner scanner = new Scanner(System.in);
+
+		// コンソールに出力
+		System.out.println("カウントダウンします。");
+
+		// 変数に代入
+		int number = scanner.nextInt();
+
+		// ループ条件
+		for (int i = 0, j = number ; i <= number; i++, j--)
+
+			// コンソールに出力
+			System.out.println(j);
+
+	}
+
+}
