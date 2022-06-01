7a0d2cca7983f56d0c323beac45613ed8a19dc87
ddt-hayata <ddt.jn.hayata@gmail.com>
2022-06-01 13:45:30 +0900

    演習7-7の課題の関数ヘッダを修正しました。

--- ensyu/src/ensyu_7_7/ensyu_7_7.java
+++ ensyu/src/ensyu_7_7/ensyu_7_7.java
@@ -7,21 +7,32 @@ package ensyu_7_7;
 import java.util.Scanner;
 
 public class ensyu_7_7 {
-
-	// 文字をn個連続表示
+	/*
+	 * 関数名：putChars
+	 * 説明：n個の'*'を連続表示するメソッド
+	 * パラメータ：putStarsメソッドから受け取った文字列 '*' 及び入力された整数値(number)
+	 * 返り値：なし
+	 * 作成者：早田
+	 * 作成日：2022/5/27
+	 */
 	static void putChars(char asterisk, int printNumber) {
 		for (int i = 1; i <= printNumber; i++) {
 			// 出力
 			System.out.print(asterisk);
 		}
 	}
-	
-	// putCharsメソッド呼び出し
+	/*
+	 * 関数名：putStars
+	 * 説明：putCharsメソッドへ文字列と入力値を渡すメソッド
+	 * パラメータ：入力された整数値(number)
+	 * 返り値：なし
+	 * 作成者：早田
+	 * 作成日：2022/5/27
+	 */
 	static void putStars(int stepsNumber) {
 		// メソッドの呼び出し
 			putChars('*', stepsNumber);
-		}
-	
+	}
 	public static void main(String[] args) {
 		// Scannerクラスのインスタンス化
 		Scanner scanner = new Scanner(System.in);
@@ -35,6 +46,7 @@ public class ensyu_7_7 {
 		
 		// 表示個数ループ
 		for (int i = 1; i <= number; i++) {
+			// メソッドの呼び出し
 			putStars(i);
 			// 改行文の出力
 			System.out.println();
