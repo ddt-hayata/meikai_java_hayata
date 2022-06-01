7b0ff72bd6d7b3bf49870c7f63cc78484a099a1f
DESKTOP-MCFR21N\jinai <ddt.jn.hayata@gmail.com>
2022-06-01 21:47:42 +0900

    コメント部分を修正しました。

--- ensyu/src/ensyu_3_16/ensyu_3_16.java
+++ ensyu/src/ensyu_3_16/ensyu_3_16.java
@@ -7,100 +7,76 @@ package ensyu_3_16;
 import java.util.Scanner;
 
 public class ensyu_3_16 {
-	
+
 	public static void main(String[] args) {
-		
+
 		// Scannerクラスのインスタンスを作成し、引数で標準入力System.inを指定
 		Scanner scanner = new Scanner(System.in);
-		
+
 		// コンソールに出力
 		System.out.print("整数a：");
-		
+
 		// 変数に代入
 		int firstNumber = scanner.nextInt();
-		
+
 		// コンソールに出力
 		System.out.print("整数b：");
-		
+
 		// 変数に代入
 		int secondNumber = scanner.nextInt();
-		
+
 		// コンソールに出力
 		System.out.print("整数c：");
-		
+
 		// 変数に代入
 		int thirdNumber = scanner.nextInt();
-		
-		if (firstNumber < secondNumber && firstNumber < thirdNumber) {
-			
-			// コンソールに出力
-			System.out.println(firstNumber);
-			
-			if (secondNumber < thirdNumber) {
-				
-				// コンソールに出力
-				System.out.println(secondNumber);
-				
-				// コンソールに出力
-				System.out.println(thirdNumber);
-				
-			} else {
-				
-				// コンソールに出力
-				System.out.println(thirdNumber);
-				
-				// コンソールに出力
-				System.out.println(secondNumber);
-			}
-			
-		} else if (secondNumber < firstNumber && secondNumber < thirdNumber) {
-			
-			// コンソールに出力
-			System.out.println(secondNumber);
-			
-			if (firstNumber < thirdNumber) {
-			
-				// コンソールに出力
-				System.out.println(firstNumber);
-				
-				// コンソールに出力
-				System.out.println(thirdNumber);
-							
-			} else {
-							
-				// コンソールに出力
-				System.out.println(thirdNumber);
-				
-				// コンソールに出力
-				System.out.println(firstNumber);
-			}
-			
-		} else if (thirdNumber < firstNumber && thirdNumber < secondNumber) {
-			
-			// コンソールに出力
-			System.out.println(thirdNumber);
-					
-			if (firstNumber < secondNumber) {
-			
-				// コンソールに出力
-				System.out.println(firstNumber);
-				
-				// コンソールに出力
-				System.out.println(secondNumber);
-						
-			} else {
-						
-				// コンソールに出力
-				System.out.println(secondNumber);
-				
-				// コンソールに出力
-				System.out.println(firstNumber);
-			}
-			
+
+		// もし整数aが整数bより小さい場合は入れ替える。
+		if (firstNumber < secondNumber) {
+
+			// 変数に代入
+			int number = firstNumber;
+
+			// 変数に代入
+			firstNumber = secondNumber;
+
+			// 変数に代入
+			secondNumber = number;
 		}
+		// もし整数bが整数cより小さい場合は入れ替える。
+		if (secondNumber < thirdNumber) {
+
+			// 変数に代入
+			int number = secondNumber;
+
+			// 変数に代入
+			secondNumber = thirdNumber;
+
+			// 変数に代入
+			thirdNumber = number;
+		}
+
+		// 再度整数aと整数bを比較し、整数aが整数bより小さい場合は入れ替える。
+		if (firstNumber < secondNumber) {
+
+			// 変数に代入
+			int number = firstNumber;
+
+			// 変数に代入
+			firstNumber = secondNumber;
+
+			// 変数に代入
+			secondNumber = number;
+		}
+
 		// コンソールに出力
-		System.out.println("昇順(小さい順)にソートしました。");
-		
-	}
-	
+		System.out.println();
+
+		// コンソールに出力
+		System.out.println("数字を昇順となるように並び替えました。");
+
+		// コンソールに出力
+		System.out.println("結果は" + thirdNumber + " < " + secondNumber + " < " + firstNumber + "です。");
+			}
+
 }
