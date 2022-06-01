9d8de790a0b614139d69f8d1124dad032e5ea00b
DESKTOP-MCFR21N\jinai <ddt.jn.hayata@gmail.com>
2022-06-01 21:27:14 +0900

    コメント部分を修正しました。

--- ensyu/src/ensyu_3_13/ensyu_3_13.java
+++ ensyu/src/ensyu_3_13/ensyu_3_13.java
@@ -9,58 +9,54 @@ import java.util.Scanner;
 public class ensyu_3_13 {
 
 	public static void main(String[] args) {
-		
+
 		// Scannerクラスのインスタンスを作成し、引数で標準入力System.inを指定
 		Scanner scanner = new Scanner(System.in);
-		
+
 		// コンソールに出力
 		System.out.print("整数a：");
-		
+
 		// 変数に代入
 		int firstNumber = scanner.nextInt();
-		
+
 		// コンソールに出力
-			System.out.print("整数b：");
-		
+		System.out.print("整数b：");
+
 		// 変数に代入
 		int secondNumber = scanner.nextInt();
-		
+
 		// コンソールに出力
 		System.out.print("整数c：");
-				
+
 		// 変数に代入
 		int thirdNumber = scanner.nextInt();
-		
-		if (secondNumber < firstNumber && firstNumber < thirdNumber) {
-			// コンソールに出力
-			System.out.print("中央値は" + firstNumber + "です。");
-		
-		} else if (thirdNumber < firstNumber && firstNumber < secondNumber) {
-			// コンソールに出力
-			System.out.print("中央値は" + firstNumber + "です。");
-		
-		} else if (firstNumber < secondNumber && secondNumber < thirdNumber) {
-			// コンソールに出力
-			System.out.print("中央値は" + secondNumber + "です。");
-		
-		} else if (thirdNumber < secondNumber && secondNumber < firstNumber) {
-			// コンソールに出力
-			System.out.print("中央値は" + secondNumber + "です。");
-		
-		} else if (firstNumber < thirdNumber && thirdNumber < secondNumber) {
-			// コンソールに出力
-			System.out.print("中央値は" + thirdNumber + "です。");
-		
-		} else if (secondNumber < thirdNumber && thirdNumber < firstNumber) {
-			// コンソールに出力
-			System.out.print("中央値は" + thirdNumber + "です。");
-		
-		} else if (secondNumber == firstNumber && firstNumber == thirdNumber) {
-			// コンソールに出力
-			System.out.print("中央値は" + firstNumber + "です。");
-		
+
+		// 中央値変数
+		int median = 0;
+
+		// 中央値を求める制御文
+		if (firstNumber >= secondNumber) {
+			if (secondNumber >= thirdNumber) {
+				// 変数に代入
+				median = secondNumber;
+			} else if (firstNumber <= thirdNumber) {
+				// 変数に代入
+				median = firstNumber;
+			} else {
+				// 変数に代入
+				median = thirdNumber;
+			}
+		} else if (firstNumber > thirdNumber) {
+			// 変数に代入
+			median = firstNumber;
+		} else if (secondNumber > thirdNumber) {
+			// 変数に代入
+			median = thirdNumber;
+		} else {
+			// 変数に代入
+			median = secondNumber;
 		}
-		
+		System.out.print("中央値は" + median + "です。");
 	}
 
 }
