6b879813c885f976caa671ceb7f6683a65741727
ddt-hayata <ddt.jn.hayata@gmail.com>
2022-06-02 09:45:48 +0900

    コメント部分の修正が完了しました。

--- ensyu/src/ensyu_3_17/ensyu_3_17.java
+++ ensyu/src/ensyu_3_17/ensyu_3_17.java
@@ -13,24 +13,18 @@ public class ensyu_3_17 {
 		
 		// Randomクラスのインスタンスを作成
 		Random random = new Random();
-		
 		// 変数に代入
 		int number = random.nextInt(3);
 		
 		if (number == 0) {
-			
 			// コンソールに出力
 			System.out.print("グー");
-		
 		} else if (number == 1) {
-			
 			// コンソールに出力
 			System.out.print("チョキ");
-		
-		} else
+		} else {
 			// コンソールに出力
 			System.out.print("パー");
-
+		}
 	}
-
 }
