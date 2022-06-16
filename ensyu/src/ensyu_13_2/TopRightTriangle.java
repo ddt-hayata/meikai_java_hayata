cc027c629009331843570ddcaf1ce3baa182bc2d
ddt-hayata <ddt.jn.hayata@gmail.com>
2022-06-16 18:22:06 +0900

    演習13-2の課題が完了しました。

---
+++ ensyu/src/ensyu_13_2/TopRightTriangle.java
@@ -0,0 +1,46 @@
+package ensyu_13_2;
+
+// 右上直角二等辺三角形を描画するクラス
+public class TopRightTriangle extends IsoscelesRightTriangle {
+	
+	// コンストラクタ
+	public TopRightTriangle(int height) {
+		super(height);
+	}
+	
+	/* 
+	 * 関数名：toString
+	 * 説明：図形情報の表示をする文字列を返却するメソッド
+	 * パラメータ：なし
+	 * 返り値：文字列
+	 * 作成者：早田
+	 * 作成日：2022/6/16
+	 */
+	public String toString() {
+		// 返却
+		return "TopRightTriangle(height：" + getHeight() + ")";
+	}
+	/* 
+	 * 関数名：draw
+	 * 説明：図形情報の表示と図形を描画するメソッド
+	 * パラメータ：なし
+	 * 返り値：文字列
+	 * 作成者：早田
+	 * 作成日：2022/6/16
+	 */
+	public void draw() {
+		for (int i = 1; i <= getHeight(); i++) {
+			for (int j = 1; j <= getHeight() ; j++) {
+				if (j < i) {
+					// 出力
+					System.out.print(" ");
+				} else {
+					// 出力
+					System.out.print('*');
+				}
+			}
+			// 出力
+			System.out.println();
+		}
+	}
+}
