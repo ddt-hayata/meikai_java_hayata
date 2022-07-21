e883905f0fc270a40b47fd6d1683a40dd11446b8
DESKTOP-MCFR21N\jinai <ddt.jn.hayata@gmail.com>
2022-07-21 15:05:23 +0900

    指摘箇所の修正が完了しました。

--- ensyu/src/ensyu_4_13/ensyu_4_13.java
+++ ensyu/src/ensyu_4_13/ensyu_4_13.java
@@ -8,34 +8,23 @@ import java.util.Scanner;
 public class ensyu_4_13 {
 
 	public static void main(String[] args) {
-
 		// インスタンス化
 		Scanner scanner = new Scanner(System.in);
-
 		// コンソールに出力
 		System.out.println("1からnまでの和を求めます。");
-
-		// 変数の宣言
-		int n;
-
 		// コンソールに出力
 		System.out.print("nの値：");
-
+		// 変数の宣言
+		int n;
 		// 変数に代入
 		n = scanner.nextInt();
-
+		// 合計用の変数
+		int sum = 0;
 		// ループ条件
-		for (int i = 1, sum = 0; i <= n; sum += i, i++)
-
-			if (i == n) {
-
+		for (int i = n; i > 0; i--) {
 				sum += i;
-
-				// コンソールに出力
-				System.out.println("1から" + n + "までの和は"  + sum + "です。");
-
-			}
-
+		}
+		// コンソールに出力
+		System.out.println("1から" + n + "までの和は"  + sum + "です。");
 	}
-
 }
