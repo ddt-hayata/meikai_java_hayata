7516b2ed8e4d10fad1bbe00d916f6b7a1772a29e
DESKTOP-MCFR21N\jinai <ddt.jn.hayata@gmail.com>
2022-07-21 13:28:19 +0900

    指摘箇所の修正が完了しました。

--- ensyu/src/ensyu_4_6/ensyu_4_6_1.java
+++ ensyu/src/ensyu_4_6/ensyu_4_6_1.java
@@ -10,40 +10,24 @@ import java.util.Scanner;
 public class ensyu_4_6_1 {
 
 	public static void main(String[] args) {
-		
 		// Scannerクラスのインスタンスを作成し、引数で標準入力System.inを指定
 		Scanner scanner = new Scanner(System.in);
-		
 		// コンソールに出力
 		System.out.print("何個*を表示しますか？：");
-		
 		// 変数に代入
 		int number = scanner.nextInt();
-		
 		// 変数の初期化
 		int i = 0;
-		
 		// ループ条件
-		
-		do { 
-			// コンソールに出力
-			System.out.print("");
-		
-		} while (number != 0);
-		
-		while (i < number) {
-			
-			// コンソールに出力
-			System.out.println('*');
-			
-			// 変数に加数
-			i++;
-			
+		if (number > 0) {
+			while (i < number) {
+				// コンソールに出力
+				System.out.print('*');
+				// 変数に加数
+				i++;
+			}
+			// 改行文の出力
+			System.out.println();
 		}
-		
-		// コンソールに出力
-		//System.out.print("");
-		
 	}
-
 }
