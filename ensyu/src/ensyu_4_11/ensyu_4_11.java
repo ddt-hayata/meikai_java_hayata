b3c52e19e08d012abffb23dbc383a1016559966b
DESKTOP-MCFR21N\jinai <ddt.jn.hayata@gmail.com>
2022-07-21 14:45:05 +0900

    指摘箇所の修正が完了しました。

--- ensyu/src/ensyu_4_11/ensyu_4_11.java
+++ ensyu/src/ensyu_4_11/ensyu_4_11.java
@@ -8,22 +8,17 @@ import java.util.Scanner;
 public class ensyu_4_11 {
 
 	public static void main(String[] args) {
-
 		// インスタンス化
 		Scanner scanner = new Scanner(System.in);
-
 		// コンソールに出力
 		System.out.println("カウントダウンします。");
-
 		// 変数に代入
 		int number = scanner.nextInt();
-
 		// ループ条件
-		for (int i = 0, j = number ; i <= number; i++, j--)
-
+		for (int i = number; i >= 0; i--) {
 			// コンソールに出力
-			System.out.println(j);
+			System.out.println(i);
 
+		}
 	}
-
 }
